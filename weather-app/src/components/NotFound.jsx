798b2a28b27a1384a3847b7ff05be5436e0f7dda
SnIvanova <1snizhanaivanova@gmail.com>
2024-01-18 00:51:02 +0100

    animation

--- weather-app/src/components/NotFound.jsx
+++ weather-app/src/components/NotFound.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
-import jpg from "../assets/img/404.jpg";
 import { Container, Row, Col, Button } from "react-bootstrap";
-//import "../assets/css/pageNotFound.css"; // Import a CSS file for additional styling
+import jpg from "../assets/img/404.jpg";
+//import "../assets/css/pageNotFound.css"; 
 
 const PageNotFound = () => {
   const fadeIn = useSpring({
@@ -14,13 +14,13 @@ const PageNotFound = () => {
 
   return (
     <Container fluid className="d-flex align-items-center justify-content-center h-100">
-      <animated.div style={fadeIn}>
+      <animated.div style={fadeIn} className="p-4">
         <Row className="text-center">
           <Col xs={12} md={6} className="mb-4">
             <img src={jpg} alt="404 page not found" className="img-fluid" />
           </Col>
           <Col xs={12} md={6} className="mb-4">
-            <h1 className="display-4">Oops! Page not found.</h1>
+            <h1 className="display-4 fw-bold text-primary">Oops! Page not found.</h1>
             <p className="lead">
               The page you are looking for might be in another dimension.
             </p>
@@ -35,4 +35,3 @@ const PageNotFound = () => {
 };
 
 export default PageNotFound;
-
